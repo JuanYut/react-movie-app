868865719c67277c33b63d6ee5b2508b7cd71527
Juan Santillan <juanyut97@hotmail.com>
2026-02-17 09:05:10 -0600

    Trending Movies Feature

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,31 +1,28 @@
 // import { Client, Databases, ID, Query } from "appwrite";
 import { Client, TablesDB, ID, Query } from "appwrite";
 
-const PROJECT_ID = import.meta.VITE_APPWRITE_PROJECT_ID;
-const ENDPOINT = import.meta.VITE_APPWRITE_ENDPOINT;
+const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const TABLE_ID = import.meta.env.VITE_APPWRITE_TABLE_ID;
 
-const client = new Client();
-client.setEndpoint("https://sfo.cloud.appwrite.io/v1");
-client.setProject("6993e1c00024662a06ac");
+const client = new Client().setEndpoint(ENDPOINT).setProject(PROJECT_ID);
 
 const database = new TablesDB(client);
-// export { client, db };
 
 // <------------------------------------------------------------------------>
 export const updateSearchCount = async (searchTerm, movie) => {
   // 1. Use Appwrite SDK to check if the search term exists in the database
   try {
-    const response = await database.listRows({
+    const result = await database.listRows({
       databaseId: DATABASE_ID,
       tableId: TABLE_ID,
       queries: [Query.equal("searchTerm", searchTerm)],
     });
 
     // 2. If it does, update the count
-    if (response.rows.length > 0) {
-      const row = response.rows[0];
+    if (result.rows.length > 0) {
+      const row = result.rows[0];
 
       await database.updateRow({
         databaseId: DATABASE_ID,
@@ -53,14 +50,16 @@ export const updateSearchCount = async (searchTerm, movie) => {
 };
 
 // <------------------------------------------------------------------------>
-// export const getTrendingMovies = async () => {
-//   try {
-//     const response = await database.listRows({
-//       databaseId: DATABASE_ID,
-//       tableId: TABLE_ID,
-//     });
-//     console.log(`response -> ${response}`);
-//   } catch (error) {
-//     console.error(`Trending Error Movies -> ${error}`);
-//   }
-// };
+export const getTrendingMovies = async () => {
+  try {
+    const result = await database.listRows({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_ID,
+      queries: [Query.limit(5), Query.orderDesc("count")],
+    });
+
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+  }
+};
